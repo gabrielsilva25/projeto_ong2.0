b3a1b92272483ee4fdfecff88b24bba341d1da1a
gabrielsilva25 <japalove456@gmail.com>
2023-11-02 16:58:09 -0300

    homepage

---
+++ script/index.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')()
+
+let num1 , num2
+
+function soma(num1 , num2) {
+    num1 + num2
+    return 
+}
+
+
+let operador = prompt('+')
+
+let num1 = parseFloat(prompt('digite um valor: '))
+
+let num2 = parseFloat(prompt('digite outro valor: '))
+
+let resultado
+
+
+
+console.log("o resultado da soma é : " , resultado)
